072b6da9daeceb24846721a00bbe4e400b3d92d4
Cissa Malheiros <cissamalheiros95@outlook.com>
2024-10-29 21:58:44 -0300

    :sparkles: feat: Add Swagger for API documentation

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import storeRoutes from './routes/storeRoutes';
-import { createTable } from './models/storeModel';
+import { setupSwagger } from './swagger';
 
 dotenv.config();
 
@@ -10,9 +10,10 @@ app.use(express.json());
 
 app.use('/api', storeRoutes);
 
+setupSwagger(app);
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, async () => {
-  await createTable();
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
