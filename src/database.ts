2321f292a11ec43ad9bac8773e02fb5f10ee1434
Cissa Malheiros <cissamalheiros95@outlook.com>
2024-10-30 21:57:57 -0300

    chore: Move database.db to data folder

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
+import path from 'path';
 
 export async function openDb() {
   return open({
-    filename: './database.db',
+    filename: path.join(__dirname, '../data/database.db'),
     driver: sqlite3.Database
   });
 }
